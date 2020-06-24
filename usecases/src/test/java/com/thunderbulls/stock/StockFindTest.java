5cc05471c9489cfd8f1195174fe0879fa3922b32
Carlos <carlos.bergamasco@gmail.com>
2020-06-24 00:40:17 -0300

    Add more test cases and initializing mock repository and finder usecase

--- usecases/src/test/java/com/thunderbulls/stock/StockFindTest.java
+++ usecases/src/test/java/com/thunderbulls/stock/StockFindTest.java
@@ -1,15 +1,36 @@
 package com.thunderbulls.stock;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class FindStockTest {
+public class StockFindTest {
+	
+	FindStock finder;
+	
+	@Before
+	public void initialize() {
+		finder = new FindStock();
+		StockRepository sr = new MockStockRepository();
+		finder.setRepository(sr);
+	}
 
 	@Test
-	public void notFoundCodeAAAA1(){
-		FindStock stock = new FindStock();
-		assertNull("Stock AAAA1 not found", stock.findByCode("AAAA1"));
-    }
+	public void canFindStockByCode() {
+		assertNotNull(finder.findByCode("AAAA1"));
+	}
+	
+	@Test
+	public void canFindStockById() {
+		assertNotNull(finder.findById(1));
+	}
+	
+	@Test
+	public void canFindStockByCorp() {
+		assertTrue(finder.findByCorpName("Petrobras").size() == 2);
+	}
+
 
 }
