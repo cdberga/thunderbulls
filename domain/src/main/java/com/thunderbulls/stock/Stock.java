4268141bd7d81c1c87a4e18a746692b6bbd077b1
Carlos <carlos.bergamasco@gmail.com>
2020-06-25 15:09:07 -0300

    Add companyName attribute and new constructor

--- domain/src/main/java/com/thunderbulls/stock/Stock.java
+++ domain/src/main/java/com/thunderbulls/stock/Stock.java
@@ -3,8 +3,14 @@ package com.thunderbulls.stock;
 public class Stock {
 
 	private String code;
+	private String companyName;
 
 	public Stock(String code) {
 		this.code = code;
+	}
+
+	public Stock(String code, String company) {
+		this.code = code;
+		this.companyName = company;
 	}
 }
