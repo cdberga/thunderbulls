ad16b03bfde51133e0efa5a8bf54b3044e1793bd
Carlos <carlos.bergamasco@gmail.com>
2020-07-06 12:12:38 -0300

    Change database entity serializable, to create a file database

--- adapters/src/main/java/com/thunderbulls/adapter/data/StockDataEntity.java
+++ adapters/src/main/java/com/thunderbulls/adapter/data/StockDataEntity.java
@@ -1,10 +1,24 @@
 package com.thunderbulls.adapter.data;
 
-public class StockDataEntity {
+import java.io.Serializable;
 
+public class StockDataEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String code;
 	String company;
 	
+	public StockDataEntity(String code) {
+		this.code = code;
+	}
+	
+	public StockDataEntity(String code, String company) {
+		this.code = code;
+		this.company = company;
+	}
 	public String getCode() {
 		return code;
 	}
@@ -16,5 +30,28 @@ public class StockDataEntity {
 	}
 	public void setCompany(String company) {
 		this.company = company;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StockDataEntity other = (StockDataEntity) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
 	}
 }
