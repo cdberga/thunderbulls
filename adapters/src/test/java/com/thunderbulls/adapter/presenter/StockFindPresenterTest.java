683986a21d7cb29a82b0e3fcabce317a96238df0
Carlos <carlos.bergamasco@gmail.com>
2020-07-07 19:05:34 -0300

    Init vars at unit test

--- adapters/src/test/java/com/thunderbulls/adapter/presenter/StockFindPresenterTest.java
+++ adapters/src/test/java/com/thunderbulls/adapter/presenter/StockFindPresenterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.thunderbulls.ResponseModel;
@@ -12,32 +13,34 @@ import com.thunderbulls.stock.Stock;
 
 public class StockFindPresenterTest {
 
+	StockFindPresenter presenter;
+	
+	@Before
+	public void initialize() {
+		presenter = new StockFindPresenter();
+	}
+	
 	@Test
 	public void createResponseOnNullStock() {
-		StockFindPresenter p = new StockFindPresenter();
-		ResponseModel<Stock> response = p.createResponse(null, "test");
+		ResponseModel<Stock> response = presenter.createResponse(null, "test");
 		assertNull(response.getObject());
 	}
 		
 	@Test
 	public void createResponseOnNullStockList() {
-		StockFindPresenter p = new StockFindPresenter();
-		ResponseModel<List<Stock>> rm = p.createListResponse(null, null);
+		ResponseModel<List<Stock>> rm = presenter.createListResponse(null, null);
 		assertNull(rm.getObject());
 	}
 	
 	@Test
 	public void viewModelFromNullResponse() {
-		StockFindPresenter p = new StockFindPresenter();
-		StockViewModel vm = p.toViewModel(null);
+		StockViewModel vm = presenter.toViewModel(null);
 		assertNull(vm);
-
 	}
 	
 	@Test
 	public void viewModelFromNullResponseList() {
-		StockFindPresenter p = new StockFindPresenter();
-		List<StockViewModel> list = p.toViewModelList(null);
+		List<StockViewModel> list = presenter.toViewModelList(null);
 		assertNull(list);
 	}
 }
