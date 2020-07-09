66144b5c885a98d25ea7968aa85645754e7b9e92
Carlos <carlos.bergamasco@gmail.com>
2020-07-09 15:47:26 -0300

    Add interface for database connections

---
+++ adapters/src/main/java/com/thunderbulls/adapter/data/StockDatabase.java
@@ -0,0 +1,13 @@
+package com.thunderbulls.adapter.data;
+
+import java.util.List;
+
+public interface StockDatabase {
+	
+	StockDataEntity findByCode(String code);
+	
+	List<StockDataEntity> findByCompany(String company);
+	
+	StockDataEntity save(StockDataEntity dataEntity);
+
+}
